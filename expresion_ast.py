61d7906f9ab3d8797dff14128bc477698fbf6f05
Anaru03 <dele22428@uvg.edu.gt>
2025-08-03 11:54:43 -0600

    Agregar transformación para operador '+'

--- expresion_ast.py
+++ expresion_ast.py
@@ -3,6 +3,12 @@ def simplificador_expresiones(regex):
     i = 0
     while i < len(regex):
         c = regex[i]
+        if i + 1 < len(regex):
+            siguiente = regex[i + 1]
+            if siguiente == '+':
+                resultado += c + c + '*'
+                i += 2
+                continue
         resultado += c
         i += 1
     return resultado
